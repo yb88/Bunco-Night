67b91fc4698266c59d22daf94f08ca640aa87b16
Youcef Bouhafna <bouhafnayoucef@gmail.com>
2016-11-10 15:51:35 -0700

    Initial Commit

---
+++ MainInterfaceVC.swift
@@ -0,0 +1,38 @@
+//
+//  MainInterfaceVC.swift
+//  Bunco Night
+//
+//  Created by youcef bouhafna on 11/8/16.
+//  Copyright © 2016 Youcef. All rights reserved.
+//
+
+import UIKit
+
+class MainInterfaceVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    
+    
+    
+}
